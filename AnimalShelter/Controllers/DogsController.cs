fa51381cdb30bc147176fee0b0c204673f009ecf
Joshua Khan <Khanjo@gmail.com>
2023-08-18 10:07:23 -0700

    add Get method for cats and dogs

---
+++ AnimalShelter/Controllers/DogsController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DogsController : ControllerBase
+    {
+        private readonly AnimalShelterContext _db;
+
+        public DogsController(AnimalShelterContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<List<Dog>> Get(string name, string breed, string color, string pattern, int maxAge)
+        {
+            IQueryable<Dog> query = _db.Dogs.AsQueryable();
+
+            if (name != null)
+            {
+                query = query.Where(entry => entry.Name == name);
+            }
+
+            if (breed != null)
+            {
+                query = query.Where(entry => entry.Breed == breed);
+            }
+
+            if (color != null)
+            {
+                query = query.Where(entry => entry.Color == color);
+            }
+
+            if (pattern != null)
+            {
+                query = query.Where(entry => entry.Pattern == pattern);
+            }
+
+            if (maxAge > 0)
+            {
+                query = query.Where(entry => entry.Age <= maxAge);
+            }
+
+            return await query.ToListAsync();
+        }
+    }
+}
