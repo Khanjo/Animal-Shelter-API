84f2c9523a7a118acb3aa8bd40d39b4f6247225a
Joshua Khan <Khanjo@gmail.com>
2023-08-18 09:38:52 -0700

    add classes and properties to models

---
+++ AnimalShelter/Models/Cat.cs
@@ -0,0 +1,13 @@
+namespace AnimalShelter.Models
+{
+    public class Cat
+    {
+        public int CatId { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public string Pattern { get; set; }
+        public int Age { get; set; }
+        public string Bio { get; set; }
+    }
+}
---
+++ AnimalShelter/Models/Dog.cs
@@ -0,0 +1,13 @@
+namespace AnimalShelter.Models
+{
+    public class Dog
+    {
+        public int DogId { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public string Pattern { get; set; }
+        public int Age { get; set; }
+        public string Bio { get; set; }
+    }
+}
